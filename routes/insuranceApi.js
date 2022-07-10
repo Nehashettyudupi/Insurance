8354b4aa0fcde6263b69cf7082f29a940aa8e13e
Neha <nehakshetty@shrewdify.com>
2022-07-10 19:53:20 +0530

    Api Updation

--- routes/insuranceApi.js
+++ routes/insuranceApi.js
@@ -4,8 +4,7 @@ const Sequelize = require('sequelize');
 const { QueryTypes } = require('sequelize');
 const customer = require('../model/customer');
 const sequelize = require('../database');
-router.post('/getInsuranceList', async function getInsuranceProduct(req,res){
-    console.log('req.body.id', req.body.id);
+router.get('/getInsuranceList', async function getInsuranceProduct(req,res){
     try{
         // let query = await customer.findAll({raw: true});
         let query = 'SELECT *,fuel.name as fuel, vehiclesegments.name as vehiclesegments FROM insurance LEFT JOIN customers ON customers.customerId = insurance.customerId LEFT JOIN fuel ON fuel.fuelId = insurance.fuelId LEFT JOIN vehiclesegments ON vehiclesegments.vehicleId = insurance.vehicleId';
@@ -13,8 +12,41 @@ router.post('/getInsuranceList', async function getInsuranceProduct(req,res){
         let result = await sequelize.query(query, {
             raw: true,
           });
-          console.log(result[0]);
+        //   console.log(result[0]);
         return res.send(JSON.stringify({data: result[0]}))}
+    catch(error){
+        console.log(error);
+        return res.sendStatus(403);
+    }
+});
+
+router.post('/saveInsuranceDetails', async function saveInsuranceDetails(req,res){
+    try{
+        console.log(req.body);
+        let queryInsurance = 'UPDATE insurance SET premium = :premium , bodilyInjuryLiability = :bodilyInjuryLiability,personalInjuryProtection = :personalInjuryProtection, propertyDamageLiability = :propertyDamageLiability, collision = :collision WHERE policyId = :policyId';
+        await sequelize.query(queryInsurance, {
+            replacements: {
+                premium: req.body.premium,
+                bodilyInjuryLiability: req.body.bodilyInjuryLiability,
+                personalInjuryProtection: req.body.personalInjuryProtection,
+                propertyDamageLiability: req.body.propertyDamageLiability,
+                collision: req.body.collision,
+                policyId: req.body.policyId,
+            },
+            raw: true,
+          });
+        let queryCustomer = 'UPDATE customers SET customerGender=:customerGender, customerIncomeGroup=:customerIncomeGroup,customerRegion=:customerRegion,customerMaritalStatus=:customerMaritalStatus WHERE customerId =:customerId';
+        await sequelize.query(queryCustomer, {
+            replacements: {
+                customerGender: req.body.customerGender,
+                customerIncomeGroup: req.body.customerIncomeGroup,
+                customerRegion: req.body.customerRegion,
+                customerMaritalStatus: req.body.customerMaritalStatus,
+                customerId:req.body.customerId,
+            },
+            raw: true,
+          });
+        return res.send(JSON.stringify('Updated Successfully'))}
     catch(error){
         console.log(error);
         return res.sendStatus(403);
