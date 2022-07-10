d0e58be3b87b620664e9dd6b84ac23cb30e9bd97
Neha <nehakshetty@shrewdify.com>
2022-07-10 11:39:08 +0530

    Router Updated

--- insurance/src/App.js
+++ insurance/src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { Insurance } from './Components/insurance';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container mt-2" style={{ marginTop: 40 }}>
+        <Routes>
+          <Route exact path="/"  element={<Insurance/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ insurance/src/Components/insurance.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+export const Insurance = () => {
+    return <div>
+        <table>
+            <thead> <tr>
+              <th>id</th>
+              <th>Channel Name</th>
+              <th>Guid</th>
+              <th>Mode</th>
+              <th>View</th>
+            </tr></thead><tbody>
+                <tr><td>id</td><td>channel name</td>
+                <td>guid</td><td>Mode</td><td>View</td></tr>
+            </tbody>
+        </table>
+    </div>
+}
