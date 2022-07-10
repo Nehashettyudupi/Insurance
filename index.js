fe3750bd193375e0ba85ccc2754cdd967b95f3a6
Neha <nehakshetty@shrewdify.com>
2022-07-10 13:46:36 +0530

    Table View Update and url calls

--- index.js
+++ index.js
@@ -14,12 +14,29 @@ const bodyParser = require('body-parser');
 //   host: 'localhost',
 //   dialect:  'mysql',
 // });
+app.use(function headerFn(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type,Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type,Accept, Authorization'
+    );
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    return res.status(200).json({});
+  }
+  next();
+});
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({  extended: false }));
 async function authenticate(){
   try {
     await sequelize.authenticate();
-    await sequelize.sync({force:true})
+    await sequelize.sync();
     // sequelize.sync({force:true});
     console.log('Connection has been established successfully.');
   } catch (error) {
--- routes/insuranceApi.js
+++ routes/insuranceApi.js
@@ -6,9 +6,9 @@ const customer = require('../model/customer');
 router.post('/getInsuranceList', async function getInsuranceProduct(req,res){
     console.log('req.body.id', req.body.id);
     try{
-        let query = await customer.findAll();
+        let query = await customer.findAll({raw: true});
         console.log(query);
-        return res.send(JSON.stringify('Qwert'))}
+        return res.send(JSON.stringify({data: query}))}
     catch(error){
         console.log(error);
         return res.sendStatus(403);
